da03cf253965ae927c78162bc301c607b0417d40
rhizinha <142527176+namoradosdarhizinha@users.noreply.github.com>
2024-10-01 12:08:34 -0300

    zser

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 
 let aula1 = ['rhianne','giuliana','ana','monize','gabriela','julia','caio','artur','vinicius','danilo','maria','marina','gabriel','adan','felipe','joao','amanda','luis','lorena','andrew','prof geovana']
 let i = 0;
- while (i<aula1.length) { console.log(i); console.log(aula1[i]); i=i+1;}
+ while (i<aula1.length) { console.log(i); console.log(aula1[i]); i++;}
+
+ for (let i=0; i< aula1.lengt; i++){
+    console.log(i);
+    console.log(aula1[i]);
+ }
