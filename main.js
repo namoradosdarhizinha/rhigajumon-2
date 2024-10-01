a891094a19798b712c345604ad4c14f8479d3096
rhizinha <142527176+namoradosdarhizinha@users.noreply.github.com>
2024-10-01 11:59:21 -0300

    dvssd

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 
-let aula1 = ['rhianne','giuliana','ana','monize','gabriela','julia','caio','artur','vinicius','danilo','maria','marina','gabriel','adan','felipe','joao','amanda','luis','lorena','andrew']
+let aula1 = ['rhianne','giuliana','ana','monize','gabriela','julia','caio','artur','vinicius','danilo','maria','marina','gabriel','adan','felipe','joao','amanda','luis','lorena','andrew','prof geovana']
 let i = 0;
  while (i<aula1.length) { console.log(i); console.log(aula1[i]); i=i+1;}
