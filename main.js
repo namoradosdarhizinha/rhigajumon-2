74c067f93dffa4d396e5c464f63fb5eccfd0a05e
rhizinha <142527176+namoradosdarhizinha@users.noreply.github.com>
2024-10-01 11:55:22 -0300

    cvb

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 
 letaula1 = ['rhianne','giuliana','ana','monize','gabriela','julia','caio','artur','vinicius','danilo','maria','marina','gabriel','adan','felipe','joao','amanda','luis','lorena','andrew']
 let i = 0;
- while (i<10) { console.log(i); console.log(letaula1[i]) i=i+1;}
+ while (i<10) { console.log(i); console.log(letaula1[i]); i=i+1;}
